393b0e08c117a95bc5df9072d211f164dcab9278
jiangtao <xt8848@126.com>
2016-05-24 20:37:41 +0800

    delete more sentenses

--- JavaOnGithub/src/HelloWorld.java
+++ JavaOnGithub/src/HelloWorld.java
@@ -4,8 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
 	}
 
 }
